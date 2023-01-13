90fd81d00215aa9c0d55132a2ae1e6777ba2b769
Admir S <admir.s@unvi.edu.ba>
2023-01-13 18:38:31 +0100

    Add project files.

---
+++ ASP.NET.MVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ASP.NET.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ASP.NET.MVC.Data.ASPNETMVCContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ASP.NET.MVC.Data.ASPNETMVCContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
